4c73ce5c2e31a2035cca00238eadcbe563a9db4d
Miloš Lipničan <lipncianmilos@gmail.com>
2025-08-24 21:57:54 +0200

    Initial commit for VocabMaster

--- update_dependencies.sh
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Updating Litestar and related dependencies to latest versions...
-pip install --upgrade litestar sqlalchemy asyncpg aiosqlite passlib python-jose pydantic pytest pytest-asyncio httpx bcrypt
-echo Dependencies updated. Please restart your application and test flash messages again.
